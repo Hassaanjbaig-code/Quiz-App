be8e280dbbc284aa0de140f6911e81af179c59c7
Hassaajbaig-code <hassaanb000@gmail.com>
2025-09-29 23:34:07 -0700

    Fix issue of test

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,11 @@ module QuizApp
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    config.after_initialize do
+      if Rails.env.test? && defined?(ActiveRecord::Base)
+        ActiveRecord::Base.configurations = {}
+      end
+    end
 
     # Configuration for the application, engines, and railties goes here.
     #
